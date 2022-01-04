5e287e0bfd5de308c6fa0af0ed9b9c6f148f54d2
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-01-04 21:24:26 +0700

    Create circle_click.py

---
+++ n0miya/tkinter/circle_click.py
@@ -0,0 +1,27 @@
+# Left click to create circle, right click to remove circle
+
+import tkinter as tk
+
+class Circle:
+    def __init__(self, mainWindow):
+        self.mainWindow = mainWindow
+        self.tag = 0
+    
+        self.canvas = tk.Canvas(mainWindow, width = 500, height = 300, bg = "white")
+        self.canvas.grid(row = 1, column = 1)
+        self.canvas.bind('<Button-1>', self.addCircle)
+        self.canvas.bind('<Button-3>', self.removeCircle)
+    
+    def addCircle(self, event):
+        tag = str(self.tag)
+        self.canvas.create_oval(event.x + 25, event.y + 25, event.x - 25, event.y - 25, tag = tag)
+        self.tag += 1
+
+    def removeCircle(self, event):
+        tag = self.canvas.find_closest(event.x, event.y)
+        self.canvas.delete(tag)
+
+
+mainWindow = tk.Tk()
+app = Circle(mainWindow)
+mainWindow.mainloop()
